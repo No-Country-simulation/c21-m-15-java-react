40f856c55199e2313d68c6416eef61c4e39a7b04
Federico Holc <federicoholc@gmail.com>
2024-10-16 19:31:45 -0300

    Update ProtectedCall.jsx

--- frontend/src/components/ProtectedCall.jsx
+++ frontend/src/components/ProtectedCall.jsx
@@ -1,17 +1,28 @@
+/* eslint-disable react/prop-types */
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedCall({ children }) {
-  const isAuthenticated = sessionStorage.getItem(location.href) === "true";
+  const isAuthenticated = localStorage.getItem(location.href) === "true";
 
+  console.log("location en prot", location);
   console.log(
     "location href en prot",
     location.href,
     isAuthenticated,
-    sessionStorage.getItem(location.href)
+    localStorage.getItem(location.href)
   );
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+    return (
+      <Navigate
+        to="/login"
+        state={{
+          from: location.pathname,
+          origin: location.origin,
+        }}
+        replace
+      />
+    );
   }
   // return <Navigate to="/login" state={{ from: location.pathname }} replace />;
   return children;
