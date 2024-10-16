541df00e327abde6516a41b2f9d01f99a0d116c8
tamara zarate <tatyy661@gmail.com>
2024-10-16 17:58:09 -0300

    rresuelvo conflicto de app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
-<<<<<<< HEAD
-import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
-=======
 import Navbar from "./components/Navbar.jsx";
->>>>>>> 819b2d3c28304666013ad5d61d979e116c73c9a3
 import CardSection from "./components/CardSection";
 import Cartilla from "./components/Cartilla";
 import VideoConsultas from "./components/VideoConsultas";
 import GestionOnline from "./components/GestionOnline";
 import SobreNosotros from "./components/SobreNosotros";
 import NuestrasEspecialidades from "./components/NuestrasEspecialidades";
-<<<<<<< HEAD
-=======
 import LoginScreen from "./components/LoginScreen";
 import ProtectedRoute from "./components/ProtectedRoute";
->>>>>>> 819b2d3c28304666013ad5d61d979e116c73c9a3
 import "./App.css";
 import Footer from "./views/Footer";
 import Carrucel from "./components/Carrucel.jsx";
 
-<<<<<<< HEAD
-function Inicio() {
-  return (
-    <>
-      <Home />
-      <SobreNosotros />
-      <CardSection />
-      <NuestrasEspecialidades />
-      
-    </>
-  );
-}
-=======
->>>>>>> 819b2d3c28304666013ad5d61d979e116c73c9a3
 function App() {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/cartilla" element={<Cartilla />} />
-        <Route path="/video-consultas" element={<VideoConsultas />} />
-        <Route path="/gestion-online" element={<GestionOnline />} />
-      </Routes>
-      <Footer />
-=======
       <Home>
         <Routes>
           <Route
@@ -82,7 +51,6 @@ function App() {
         </Routes>
         <Footer />
       </Home>
->>>>>>> 819b2d3c28304666013ad5d61d979e116c73c9a3
     </>
   );
 }
