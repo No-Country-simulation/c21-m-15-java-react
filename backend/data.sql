dd48461af7695eab03ccc719f78c881bf34966ff
Chenhao <chenhaohu555@gmail.com>
2024-10-17 18:15:59 +0200

    Added data.sql

---
+++ backend/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO _user (id, password, role, username) VALUES
+(1, '$2a$12$iK5eDZFHbXcjwQ6QLstNr.eXfBTwrNg.ejwv0Z1PEzwg9gRbXup8O', 'PATIENT', 'john_doe'),
+(2, '$2a$12$eco5vfdU0jU1nZ0K.IaqCeSVgQY6X.0s3uRcFcXT1aGUyuRQfyn1W', 'PATIENT', 'jane_smith'),
+(3, '$2a$12$7TKVmOmH9t.0XI/Pzr225OfS5L4ZkxTVekBxZsRwGYw97symXyx8a', 'PATIENT', 'emma_jones');
