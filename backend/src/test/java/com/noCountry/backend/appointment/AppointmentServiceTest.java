172995ede4a82434c9508a9408891402b843bb62
Chenhao <chenhaohu555@gmail.com>
2024-10-27 10:15:45 +0100

    Deleted test

--- backend/src/test/java/com/noCountry/backend/appointment/AppointmentServiceTest.java
+++ backend/src/test/java/com/noCountry/backend/appointment/AppointmentServiceTest.java
@@ -160,19 +160,4 @@ public class AppointmentServiceTest {
         verify(openingHourRepository, times(1)).findAll();
         verify(appointmentRepository, atLeastOnce()).save(any(Appointment.class));
     }
-
-    @Test
-    public void testDeleteUnbookedPastAppointments() {
-        LocalDateTime now = LocalDateTime.now();
-
-        Appointment appointment = new Appointment();
-        appointment.setStartDateTime(now.minusDays(1));
-        appointment.setBooked(false);
-
-        when(appointmentRepository.findPastAppointmentsByIsBooked(now, false)).thenReturn(List.of(appointment));
-
-        service.deleteUnbookedPastAppointments();
-
-        verify(appointmentRepository).delete(appointment);
-    }
 }
